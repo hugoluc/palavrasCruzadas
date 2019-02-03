b3fcb1428e1b73ab44e370a6e3e932c2760948e0
hugoluc <hugoglucena@gmail.com>
2019-02-03 02:05:55 -0200

    adding slides

--- classes/aditionalInfoPage.js
+++ classes/aditionalInfoPage.js
@@ -1,6 +1,10 @@
 function aditionalInfoPage(){
 
-  var proportions = {
+
+  this.imageIndex = -1
+  this.imageTimer = 4500
+
+  this.proportions = {
     height : (6/10),
     width : (2/3),
     padding : canvasSize.width * 0.05
@@ -21,25 +25,25 @@ function aditionalInfoPage(){
   this.images.id = "images"
   this.images.style.width = canvasSize.width + "px"
   this.images.style.transition = "opacity " + transitionTime + "s"
-  this.images.style.height = canvasSize.height * (1 - proportions.height ) - (1.5 * proportions.padding) + "px"
+  this.images.style.height = canvasSize.height * (1 - this.proportions.height ) - (1.5 * this.proportions.padding) + "px"
   this.container.appendChild(this.images)
 
   //Title
   this.tittle =  document.createElement("div")
   this.tittle.id = "aditionalTitle"
   this.tittle.style.transition = "opacity " + transitionTime + "s"
-  this.tittle.style.width = (canvasSize.width * (1-proportions.width) ) - (1.5 * proportions.padding) + "px"
-  this.tittle.style.height = (canvasSize.height * proportions.height) - (1.5 * proportions.padding) + "px"
-  this.tittle.style.padding = proportions.padding+ "px"
+  this.tittle.style.width = (canvasSize.width * (1-this.proportions.width) ) - (1.5 * this.proportions.padding) + "px"
+  this.tittle.style.height = (canvasSize.height * this.proportions.height) - (1.5 * this.proportions.padding) + "px"
+  this.tittle.style.padding = this.proportions.padding+ "px"
   this.tittle.style.fontSize = canvasSize.width * 0.05 + "px"
   this.container.appendChild(this.tittle)
 
   //Content
   this.text = document.createElement("div")
   this.text.id = "aditionalText"
-  this.text.style.width = (canvasSize.width * proportions.width) - (1.5 * proportions.padding) + "px"
-  this.text.style.height = (canvasSize.height * proportions.height) - (1.5 * proportions.padding) + "px"
-  this.text.style.margin = proportions.padding+ "px"
+  this.text.style.width = (canvasSize.width * this.proportions.width) - (1.5 * this.proportions.padding) + "px"
+  this.text.style.height = (canvasSize.height * this.proportions.height) - (1.5 * this.proportions.padding) + "px"
+  this.text.style.margin = this.proportions.padding+ "px"
   this.text.style.marginLeft = "0px"
   this.text.style.fontSize = canvasSize.width * 0.03 + "px"
   this.text.style.transition = "opacity " + transitionTime + "s"
@@ -87,10 +91,13 @@ aditionalInfoPage.prototype.init = function(_data,_callBack){
 
     this.images.style.opacity = 1
     this.images.style.transform = "translateX(0px)"
-    this.images.style.backgroundImage = "url(imagens/" + _data.imagens[0] + ")"
+    // this.images.style.backgroundImage = "url(imagens/" + _data.imagens[0] + ")"
 
     this.backBtn.style.opacity = 1
     this.backBtnContainer.style.opacity = 1
+    // debugger
+    this.addslide(this.data.imagens)
+
   },100)
 
 }
@@ -123,5 +130,95 @@ aditionalInfoPage.prototype.back = function(){
   },500)
 
   this.reset()
+
+}
+
+
+aditionalInfoPage.prototype.addslide = function(_images){
+
+  this.removeSlide()
+
+  var sizes = {
+    controlCircle : 35,
+    controlGap : 20,
+    imageHeight : canvasSize.height * (1 - this.proportions.height ) - (1.5 * this.proportions.padding)
+  }
+
+  var controlWidth = _images.length * (sizes.controlCircle + sizes.controlGap)
+
+  var imageContainer = document.createElement("div")
+  imageContainer.id = "imageContainer"
+  this.images.appendChild(imageContainer)
+
+  var controlsContainer = document.createElement("div")
+  controlsContainer.style.width = controlWidth + "px"
+  controlsContainer.style.top = (_images.length * sizes.controlCircle) + ((_images.length-1) * sizes.controlGap) + "px"
+  controlsContainer.style.position = "absolute"
+  controlsContainer.style.display = "flex"
+  controlsContainer.style.top = sizes.imageHeight - sizes.controlCircle - 30 + "px"
+  controlsContainer.style.left = (canvasSize.width/2) - (controlWidth/2) + "px"
+  controlsContainer.style.zIndex = 1
+  controlsContainer.id = "circles"
+  this.images.appendChild(controlsContainer)
+
+  for (var i = 0; i < _images.length; i++) {
+
+    var image = document.createElement("div")
+    image.id = "aditionalInfo_Contaiter"
+    image.style.backgroundImage = "url(imagens/" + _images[i] + ")"
+    image.style.backgroundSize = "cover"
+    image.style.height = "100%"
+    image.style.width = "100%"
+    image.style.transition = "opacity 1s"
+    imageContainer.appendChild(image)
+
+    var circle = document.createElement("div")
+    circle.style.width = sizes.controlCircle + "px"
+    circle.style.height = sizes.controlCircle + "px"
+    circle.style.borderRadius = sizes.controlCircle + "px"
+    circle.style.marginRight = sizes.controlGap + "px"
+    circle.style.background = "rgb("+globalColors.gray.r+","+globalColors.gray.g+","+globalColors.gray.b+")"
+    circle.style.boxShadow = "rgba(0, 0, 0, 0.2) 1px 2px 40px 10px"
+    circle.style.transition = "background 0.5s"
+    controlsContainer.appendChild(circle)
+
+  }
+
+  this.changeImage()
+  this.slideActive = true
+
+  setTimeout ( () => {
+    if(this.slideActive == true) this.slideLoop()
+  }, this.imageTimer)
+
+}
+
+aditionalInfoPage.prototype.removeSlide = function(_images){
+  this.images.innerHTML = ""
+  this.slideActive = false
+}
+
+aditionalInfoPage.prototype.slideLoop = function(_images){
+  if(this.slideActive == true){
+    this.changeImage()
+    setTimeout( () => { this.slideLoop() }, this.imageTimer)
+  }
+
+}
+
+aditionalInfoPage.prototype.changeImage = function(_images){
+
+
+  var images = this.images.querySelector("#imageContainer")
+  var circles = this.images.querySelector("#circles")
+  for (var i = 0; i < images.children.length; i++) {
+    images.children[i].style.opacity = 0
+    circles.children[i].style.background = "rgb("+globalColors.gray.r+","+globalColors.gray.g+","+globalColors.gray.b+")"
+  }
+
+  this.imageIndex = this.imageIndex == images.children.length-1 ? 0 : this.imageIndex + 1
+  images.children[this.imageIndex].style.opacity = 1
+  circles.children[this.imageIndex].style.background = "white"
+
 
 }
